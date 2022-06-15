0d4291d7401ab40fefb7d7d3afe4535efd8f7b14
svhrkumar544 <52437024+svhrkumar544@users.noreply.github.com>
2022-06-15 09:49:58 +0530

    airports schema changes

--- schemas/airportsData.js
+++ schemas/airportsData.js
@@ -3,8 +3,8 @@ const mongoDB = require('mongoose');
 const airportsCity = mongoDB.Schema({
 	IATA_code: { type: String, default: null },
 	ICAO_code: { type: String, default: null },
-	airportName: { type: String, default: null },
-	cityName: { type: String, default: null },
+	airport_name: { type: String, default: null },
+	city_name: { type: String, default: null },
 });
 
 module.exports = mongoDB.model('airportsData', airportsCity);
